df5adb89601c77844799ca3e07c85be59a9ba5ff
bituparnabarman50-oss <bituparnabarman50@gmail.com>
2025-10-24 23:14:35 +0530

    Add files via upload
    n*(n+1)/2

---
+++ Assignment_1/SUM_of_first n natural numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int n,sum;
+printf("Enter the number:");
+scanf("%d", &n);
+sum=n*(n+1)/2;
+printf("The sum of the first n natural number is:%d",sum);
+return 0;
+}
