37c1b27fda82ce35c356988c0d506b938bfefc25
bituparnabarman50-oss <bituparnabarman50@gmail.com>
2025-10-25 01:25:07 +0530

    Add files via upload
    intro.

---
+++ Lab-1/introduction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+printf("my name is: BITUPARNA BARMAN \n");
+printf("my roll no is: MEB25067 \n");
+printf("my department is: MECHANICAL ENGINEERING \n ");
+printf("my university name is : TEZPUR UNIVERSITY");
+return 0;
+}
+
